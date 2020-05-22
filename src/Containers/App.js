3d16f61aac694aef5753c5e96c288446670a3de9
Natruwanni <Natruwanni@users.noreply.github.com>
2020-05-22 23:21:09 +1200

    catfriends

---
+++ src/Containers/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import CardList from '../Components/CardList';
+import SearchBox from '../Components/SearchBox';
+import Scroll from '../Components/Scroll';
+import ErrorBoundry from '../Components/ErrorBoundry';
+import './App.css';
+
+class App extends Component  {
+	constructor() {
+		super()
+		this.state = {
+			Cats: [],
+			searchfield: ''
+		}
+	}
+
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then(response => response.json())
+			.then(users => this.setState({ Cats: users }));
+	}
+
+	onSearchChange = (event) => { 
+		this.setState({ searchfield: event.target.value })
+	}
+
+
+	render() {
+		const { Cats, searchfield } = this.state;
+			const filteredCats = Cats.filter(Cats => {
+			return Cats.name.toLowerCase().includes(searchfield.toLowerCase());
+		})
+		return !Cats.length ? 
+				<h1>Loading</h1> :
+				(
+				<div className='tc'>
+					<h1>Cat Friends</h1>
+					<SearchBox searchChange={this.onSearchChange}/>
+					<Scroll>
+						<ErrorBoundry>
+						<CardList Cats={filteredCats}/>
+						</ErrorBoundry>
+					</Scroll>
+				</div>
+				);
+	}
+	
+}
+
+
+export default App;
